a1e7fd69d2e1690f695d41939e351c9c4790b4d4
gonen.g <Gonen.Goshen@solaredge.com>
2019-11-28 11:45:45 +0200

    [git] Add git-alias for cherry-pick

--- git/source_git_aliases.bash
+++ git/source_git_aliases.bash
@@ -1,6 +1,7 @@
 git config --global alias.alias '! git config --get-regexp ^alias\. | sort'
 git config --global alias.aliases '! git config --get-regexp ^alias\. | sort'
 git config --global alias.co checkout
+git config --global alias.cp cherry-pick
 git config --global alias.last 'log -1 HEAD'
 git config --global alias.lg 'log --decorate --oneline --graph'
 git config --global alias.lga 'log --decorate --oneline --graph --all'
