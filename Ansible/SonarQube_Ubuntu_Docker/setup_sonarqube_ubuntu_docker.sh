f0d9954361ac2c7d88753af4f4d37f348fd33914
Gonen <830775+goneng@users.noreply.github.com>
2024-11-02 17:07:30 +0200

    [SonarQube] WIP: Setup as Docker on Ubuntu
    - Add Host-Setup per 'Pre-Installation' tips
       - https://docs.sonarsource.com/sonarqube/latest/setup-and-upgrade/pre-installation/linux/
       - Pre-Installation > Configuring the maximum number of open files and other limits
    - Fix the inventory, add a shell-wrapper
    - Fix ssh-connection, fix the shell-wrapper
    - Fix docker-compose version and arch.
    - Fix docker-compose version and arch.
    - Delay PostgreSQL errors (maybe we don't need it?)
    - Confirm that 'seccomp' is available on the kernel
    - Create some docker-volumes
    - Create docker-compose file
    - Run the docker-compose file on the remote host
    - Fix the final validation (confirms the site is up)
    - Create the docker-compose file from a template
    - Set the containers to restart after reboot (or failure)
    - Move some folders around

---
+++ Ansible/SonarQube_Ubuntu_Docker/setup_sonarqube_ubuntu_docker.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+# Set default values for parameters, case they are empty or undefined
+: "${GIT_ROOT:=$(git rev-parse --show-toplevel)}"
+: "${SCRIPT_HOME:=$GIT_ROOT/Ansible/SonarQube_Ubuntu_Docker/}"
+: "${SERVER:=sonarqube-test}"
+
+SCRIPT_NAME="$(basename "$0")"
+echo "--- -----------------------------------------------------------------------------------------"
+echo "-S- ${SCRIPT_NAME}: Setup SonarQube (in ${SERVER})"
+echo "--- -----------------------------------------------------------------------------------------"
+echo "-I- Running as '$(whoami)' on '$(hostname)'"
+
+source "${GIT_ROOT}/Unix_Admin/bin/devops_fun.sh"
+
+# -------------------------------------------------------------------------------------- #
+
+do_fun_validate_param        ANSIBLE_USER
+do_fun_validate_param_silent ANSIBLE_PASS
+
+if [[ ${VERBOSE:-false} != "false" ]]
+then
+  do_func_show_env_vars
+  do_func_show_command_version  python
+  do_func_show_command_version  python3
+
+  # Activate command-echo
+  set -x
+fi
+
+cd "${SCRIPT_HOME}" || exit
+
+# Prepare/use the Virtual-Env ______________________________
+VENV_DIR="${SCRIPT_NAME%.*}_venv"  # Strip the extension from the (short) filename
+
+VENV_COMMAND=virtualenv
+
+do_fun_confirm_we_have_python3_package "${VENV_COMMAND}"
+
+do_fun_reset_virtualenv_dir "${VENV_DIR}" "${RESET_VENV}"
+
+# Create the Virtual-Env (force python3)
+virtualenv -p "$(which python3)" "${VENV_DIR}"
+
+# Initialize the Virtual-Env
+source "${VENV_DIR}/bin/activate"
+
+export ANSIBLE_COLLECTIONS_PATH="${VENV_DIR}/collections"
+ansible-galaxy collection install community.docker
+ansible-galaxy collection list
+
+ansible-playbook -i inventory_sonarqube.yml setup_sonarqube_ubuntu_docker.yml \
+    --extra-vars "ANSIBLE_USER=${ANSIBLE_USER}" \
+    --extra-vars "ANSIBLE_PASS=${ANSIBLE_PASS}" \
+    ${VERBOSE} \
+    "$@"
+
+# -------------------------------------------------------------------------------------- #
+echo "-D- ${SCRIPT_NAME}: Done."
